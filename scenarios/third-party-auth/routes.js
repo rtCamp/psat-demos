5b5d5f541520818e301d50650c9d31b1cf782ed1
Fellyph Cintra <fellyph.cintra@gmail.com>
2023-12-13 15:21:08 +0000

    adding route to third-party auth

---
+++ scenarios/third-party-auth/routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+	res.render('common/under-construction', {
+		title: '🚧 Third-party Authentication'
+	});
+});
+
+module.exports = router;
