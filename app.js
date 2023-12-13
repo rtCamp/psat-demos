4740992c5d8a7484e607eb5f421e4d76e48aa809
Fellyph Cintra <fellyph.cintra@gmail.com>
2023-12-13 14:57:13 +0000

    adding new routes

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ demoTypes.forEach(demoType => {
 });
 
 // Mount routes for different scenarios
-const scenarios = ['ecommerce', 'single-sign-on', 'analytics', 'embedded-video'];
+const scenarios = ['ecommerce', 'single-sign-on', 'analytics', 'embedded-video', 'payment-flow', 'content-delivery-networks'];
 scenarios.forEach(scenario => {
 	const scenarioRoutes = require(`./scenarios/${scenario}/routes`);
 	app.use(`/${scenario}`, scenarioRoutes);  // Mount the routes on a path specific to the scenario
