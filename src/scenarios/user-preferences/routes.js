f34ca3755a3c51a3efd6dab73935a4d55795df78
Fellyph Cintra <fellyph.cintra@gmail.com>
2024-02-12 08:48:20 +0000

    Changing user preference to personalization

--- src/scenarios/user-preferences/routes.js
+++ src/scenarios/user-preferences/routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     res.render(path.join(__dirname,'index'), {
-        title: 'User Preferences'
+        title: 'Personalization'
     });
 });
 
