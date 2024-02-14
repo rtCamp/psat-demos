988de3736838cf415fc914787e39a2a3c00764a5
Fellyph Cintra <fellyph.cintra@gmail.com>
2024-02-14 14:11:49 +0000

    center element on iframe

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -5,9 +5,11 @@ document.addEventListener('DOMContentLoaded', function() {
         const mainHeader = document.querySelector('.main-header');
         const mainFooter = document.querySelector('.main-footer');
         const internalPageHeader = document.querySelector('.internal-page-header');
+        const themeContainer = document.getElementById('theme-container');
 
         mainHeader?.remove(); 
         mainFooter?.remove();
         internalPageHeader?.remove('hidden');
+        themeContainer?.classList.add('h-screen');
     }
 });
