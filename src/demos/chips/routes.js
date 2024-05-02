7cb65479af652686f7b5be6dc26dba50ba55444c
Fellyph Cintra <fellyph.cintra@gmail.com>
2024-05-02 12:40:13 +0100

    commenting router script

--- src/demos/chips/routes.js
+++ src/demos/chips/routes.js
@@ -19,6 +19,7 @@ router.get('/analytics-third-party', (req, res) => {
 
 // Serve the analytics.js file to the site
 router.get( '/analytics.js', ( req, res ) => {
+	// Check if the analyticsId cookie is set
 	let analyticsId = req.cookies.analyticsId;
 	if ( !analyticsId ) {
 		// Generate a new analytics ID
@@ -26,59 +27,69 @@ router.get( '/analytics.js', ( req, res ) => {
 
 		// Store the analytics ID in a cookie
 		res.cookie( 'analyticsId', analyticsId, {
+			// Set the domain of the cookie to the current domain
 			Domain: `.${res.locals.domainC}`,
-			maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+			// Set the max age of the cookie to 30 days
+			maxAge: 30 * 24 * 60 * 60 * 1000,
+			// Set the httpOnly flag to true to prevent the cookie from being accessed by JavaScript
 			httpOnly: true,
+			// Set the sameSite flag to "none" to allow the cookie to be sent across different sites
 			sameSite: "none",
+			// Set the secure flag to true to require the cookie to be sent over HTTPS
 			secure: true
 		} );
 
 
 	}
 
+	// Check if the analyticsId-chips cookie is set
 	let analyticsIdCHIPS = req.cookies['analyticsId-chips'];
 
 	if ( !analyticsIdCHIPS ) {
+		// If the analyticsId-chips cookie is not set, generate a new analytics ID
 		analyticsIdCHIPS = uuid.v4();
+		
+		// Store the analytics ID in a cookie
 		res.cookie( 'analyticsId-chips', analyticsIdCHIPS, {
+			// Set the domain of the cookie to the current domain
 			domain: `.${res.locals.domainC}`,
-			maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+			// Set the max age of the cookie to 30 days
+			maxAge: 30 * 24 * 60 * 60 * 1000,
+			// Set the httpOnly flag to true to prevent the cookie from being accessed by JavaScript
 			httpOnly: true,
+			// Set the sameSite flag to "none" to allow the cookie to be sent across different sites
 			sameSite: "none",
+			// Set the secure flag to true to require the cookie to be sent over HTTPS
 			secure: true,
+			// Set the partitioned flag to true to prevent the cookie from being shared across different partitions
 			partitioned: true
 		} );
 	}
 
-	// Set the appropriate content type and send the analytics code
+	// Set the Content-Type header to 'application/javascript' and render the analytics file
 	res.set( 'Content-Type', 'application/javascript' );
 	res.render(path.join(__dirname,'analytics'));
 } );
 
+// Route to track user interactions
 router.post( '/track', ( req, res ) => {
 	const {interaction} = req.body;
 	const analyticsId = req.cookies.analyticsId;
 
-	if ( interaction && analyticsId ) {
-
-		// Send success status for successful interaction tracking
-		res.status( 200 ).send( analyticsId );
-	} else {
-		res.status( 400 ).send( 'Invalid request' );
-	}
+	// If interaction and analyticsId are present, send a success status
+    // Otherwise, send a 400 status with an error message
+    interaction && analyticsId ? res.status(200).send(analyticsId) : res.status(400).send('Invalid request');
 } );
 
+// Route to track user interactions for CHIPS
 router.post( '/trackCHIPS', ( req, res ) => {
 	const {interaction} = req.body;
 	const analyticsIdCHIPS = req.cookies['analyticsId-chips'];
 
-	if ( interaction && analyticsIdCHIPS ) {
-
-		// Send success status for successful interaction tracking
-		res.status( 200 ).send( analyticsIdCHIPS );
-	} else {
-		res.status( 400 ).send( 'Invalid request' );
-	}
+	// If interaction and analyticsIdCHIPS are present, send a success status
+    // Otherwise, send a 400 status with an error message
+    interaction && analyticsIdCHIPS ? res.status(200).send(analyticsIdCHIPS) : res.status(400).send('Invalid request');
 } );
 
+// Export the router
 module.exports = router;
