b2d9f0435546adb70ca168df524645b14072533a
Riddhesh Sanghvi <riddhesh237@gmail.com>
2025-04-23 12:17:23 +0530

    Add functions to install Node dependencies and run build in deployment script

--- .github/deploy/addon.sh
+++ .github/deploy/addon.sh
@@ -9,6 +9,8 @@ function main() {
 	setup_hosts_file
 	check_branch_in_hosts_file
 	setup_ssh_access
+	maybe_install_node_dep
+	maybe_run_node_build
 	setup_files # Adding the custom function in the order where needed.
 	deploy
 }
